d561c6ef33ffa8f5d2c8ec8d7cab8f1efa05c640
Bradley Bartlett <bbartlett222@gmail.com>
2019-05-10 18:19:19 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+def reverse_each_word(string)
+  first_array = string.split(" ")
+  return_array = []
+  first_array.each do |string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
+end
+
